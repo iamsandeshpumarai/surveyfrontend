995254be29a1d9f7365e6ac8cc63f789ca4c37d4
Sandesh Rai <pumasandeshrai@gmail.com>
2026-01-12 23:35:48 +0545

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import { DataproviderWraper } from './Component/Context/ContextDataprovider.jsx'
 
 const queryClient = new QueryClient()
@@ -12,9 +12,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <DataproviderWraper>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-</BrowserRouter>
+</HashRouter>
       </DataproviderWraper>
     </QueryClientProvider>
   </StrictMode>
