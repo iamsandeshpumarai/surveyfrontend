7e9225de945184438e70499a0cd9e2cb99d74c9c
Sandesh Rai <pumasandeshrai@gmail.com>
2026-01-21 22:46:04 +0545

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL:"https://surveybackend-erv4.onrender.com/",
-    baseURL:"http://localhost:5001",
+    baseURL:"https://surveybackend-erv4.onrender.com/",
+    // baseURL:"http://localhost:5001",
     withCredentials:true
 
 })
